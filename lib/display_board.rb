5b67c63b69c5f0909c436a94d35500f4e2705bfa
Chiara Cokieng <chiara@chiaracokieng.com>
2018-08-21 07:27:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,5 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+rows = ["   |   |   ", "------------", "   |   |   ", "------------", "   |   |   "]
 def display_board(board)
   puts board
 end
